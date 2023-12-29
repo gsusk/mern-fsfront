55a8f4d31209132c7f1d29d53e39a51a918ead15
Santiago <satehortuamercado@gmail.com>
2023-12-29 17:16:49 -0500

    connect to db

--- back/index.js
+++ back/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to Mongoose")
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 const app = express()
 
 app.listen(3000, () => {
-  console.log('server listening on port ', 3000);
+  console.log("server listening on port ", 3000)
 })
