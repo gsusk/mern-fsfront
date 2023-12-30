5b022e03b3b23e16817bc46998a1bc54bb43ef84
Santiago <satehortuamercado@gmail.com>
2023-12-29 21:51:37 -0500

    create signIn route

--- back/controller/auth.controller.js
+++ back/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.models.js"
 import bcrypt from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body
@@ -14,7 +16,25 @@ export const signUp = async (req, res, next) => {
   }
 }
 
-export const signIn = async (req, res) => {
-  const { username, password } = req.body
-  const foundUser = await User.findOne(username)
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    const userFound = await User.findOne({ email })
+    if (!userFound) return next(errorHandler(404, "User not found"))
+    const validPassword = bcrypt.compareSync(password, userFound.password)
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"))
+    const token = jwt.sign({ id: userFound._id }, process.env.JWT_SECRET)
+    // eslint-disable-next-line no-unused-vars
+    const { password: pass, ...userInfo } = userFound._doc
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60),
+        secure: false,
+      })
+      .status(200)
+      .json(userInfo)
+  } catch (err) {
+    next(err)
+  }
 }
--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -4,5 +4,6 @@ import { signIn, signUp } from "../controller/auth.controller.js"
 const router = Router()
 
 router.post("/signup", signUp)
+router.post("/signin", signIn)
 
 export default router
