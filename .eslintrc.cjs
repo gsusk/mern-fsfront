27701c807d4a8a124043e88e63d0fcd6e6827178
unknown <satehortuam@gmail.com>
2023-12-30 19:48:54 -0500

    redux state functionality added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { node: true, es2020: true },
   extends: ["eslint:recommended", "prettier"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "mernstack"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["prettier"],
