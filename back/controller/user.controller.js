88daeeb5cc24a2042848e54943dd48393589a12a
unknown <satehortuamercado@gmail.com>
2024-01-01 14:46:50 -0500

    update user data feature added

--- back/controller/user.controller.js
+++ back/controller/user.controller.js
@@ -1,8 +1,38 @@
+import User from "../models/user.models"
+import { errorHandler } from "../utils/error"
+import bcrypt from "bcryptjs"
+
 export const test = (req, res) => {
   res.json({ message: "Hello world" })
 }
 
 export const updateUser = async (req, res, next) => {
-  const { id } = req.query
-  res.json()
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only update your own account"))
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10)
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          avatar: req.body.avatar,
+          password: req.body.password,
+        },
+      },
+      { new: true },
+    )
+
+    // eslint-disable-next-line no-unused-vars
+    const { password, ...rest } = updateUser._doc
+
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
 }
