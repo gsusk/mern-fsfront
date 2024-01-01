44d1cbbd3c02c0ea7d537b673dd527682bf92072
unknown <satehortuam@gmail.com>
2024-01-01 14:30:29 -0500

    added token verification

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
-import { test } from "../controller/user.controller.js"
+import { test, updateUser } from "../controller/user.controller.js"
+import { verifyToken } from "../utils/verifyUser.js"
 const router = Router()
 
 router.get("/", test)
+router.post("/update/:id", verifyToken, updateUser)
 
 export default router
