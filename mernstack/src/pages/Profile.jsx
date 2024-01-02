3e37c750edc2ba85be29c3f40b638133954932e7
unknown <satehortuamercado@gmail.com>
2024-01-02 12:22:48 -0500

    feat: listing page

--- mernstack/src/pages/Profile.jsx
+++ mernstack/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 import {
   getDownloadURL,
   getStorage,
@@ -189,6 +190,12 @@ export default function Profile() {
         >
           {loading ? "loading" : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg text-center uppercase hover:opacity-95 my-5"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
