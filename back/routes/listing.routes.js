ca04cbb1b0c9a7946c7dcabe0c280dcc493c7801
unknown <satehortuamercado@gmail.com>
2024-01-04 17:40:14 -0500

    feat: delete listing route

--- back/routes/listing.routes.js
+++ back/routes/listing.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express"
-import { createListing } from "../controller/listing.controller.js"
+import {
+  createListing,
+  deleteListing,
+} from "../controller/listing.controller.js"
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = Router()
 
 router.post("/create", verifyToken, createListing)
+router.delete("/delete", verifyToken, deleteListing)
 
 export default router
