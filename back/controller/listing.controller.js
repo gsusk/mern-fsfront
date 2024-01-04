d3e365fca10e3ae05558f40e6b02e8bd9f287717
unknown <satehortuamercado@gmail.com>
2024-01-04 17:52:31 -0500

    feat: delete user listing functonality

--- back/controller/listing.controller.js
+++ back/controller/listing.controller.js
@@ -20,7 +20,7 @@ export const deleteListing = async (req, res, next) => {
     if (req.user.id !== listing.userRef) {
       return next(errorHandler(404, "Listing not found!"))
     }
-    await Listing.findByIdAndUpdate(req.params.id)
+    await Listing.findByIdAndDelete(req.params.id)
     res.status(200).json("Listing has been deleted")
   } catch (error) {
     next(error)
